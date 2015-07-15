852e0cf372392062e782254af33d66cfb2d96411
Gavin Hamill <gavin@bashton.com>
2015-07-15 12:28:16 +0100

    Revert "support puppet_exec_path parameter with sensible default for Facter 3.x"
    puppet facts --render-as yaml doesn't produce output that mcollective can read
    
    This reverts commit 3c89bef530184f6daa5c393fc5d15ad3aa596123.

--- manifests/server/config/factsource/yaml.pp
+++ manifests/server/config/factsource/yaml.pp
@@ -12,25 +12,22 @@ class mcollective::server::config::factsource::yaml {
   }
   $yaml_fact_cron      = $mcollective::yaml_fact_cron
 
+  # Template uses:
+  #   - $ruby_shebang_path
+  #   - $yaml_fact_path_real
   if $yaml_fact_cron {
     if versioncmp($::facterversion, '3.0.0') >= 0 {
       cron { 'refresh-mcollective-metadata':
-        command => "${mcollective::puppet_exec_path}/puppet facts --render-as --yaml >${yaml_fact_path_real} 2>&1",
+        command => "facter --yaml >${yaml_fact_path_real} 2>&1",
         user    => 'root',
         minute  => [ '0', '15', '30', '45' ],
       }
       exec { 'create-mcollective-metadata':
         path    => "/opt/puppet/bin:${::path}",
-        command => "${mcollective::puppet_exec_path}/puppet facts --render-as --yaml >${yaml_fact_path_real} 2>&1",
+        command => "facter --yaml >${yaml_fact_path_real} 2>&1",
         creates => $yaml_fact_path_real,
       }
-      mcollective::server::setting { 'puppet_exec_path':
-        value => "${mcollective::puppet_exec_path}/puppet",
-      }
     } else {
-      # Template uses:
-      #   - $ruby_shebang_path
-      #   - $yaml_fact_path_real
       file { "${mcollective::site_libdir}/refresh-mcollective-metadata":
         owner   => '0',
         group   => '0',
