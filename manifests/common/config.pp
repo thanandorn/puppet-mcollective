836d882b1bad78492a42a11a1ef494d1d5f8904a
Gavin Hamill <gavin@bashton.com>
2015-07-15 12:09:14 +0100

    Try out some more Windows love to wipe away those tears of red

--- manifests/common/config.pp
+++ manifests/common/config.pp
@@ -4,65 +4,75 @@ class mcollective::common::config {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  file { $mcollective::site_libdir:
-    ensure       => directory,
-    owner        => 'root',
-    group        => '0',
-    mode         => '0644',
-    recurse      => true,
-    purge        => true,
-    force        => true,
-    source       => [],
-    sourceselect => 'all',
-  }
-
-  if $mcollective::server {
-    # if we have a server install, reload when the plugins change
-    File[$mcollective::site_libdir] ~> Class['mcollective::server::service']
-  }
-
-  datacat_collector { 'mcollective::site_libdir':
-    before          => File[$mcollective::site_libdir],
-    target_resource => File[$mcollective::site_libdir],
-    target_field    => 'source',
-    source_key      => 'source_path',
-  }
-
-  datacat_fragment { 'mcollective::site_libdir':
-    target => 'mcollective::site_libdir',
-    data   => {
-      source_path => [ 'puppet:///modules/mcollective/site_libdir' ],
+  case $::kernel {
+    'windows': {
+      $site_libdir_owner = 'Administrator'
+      $site_libdir_group = 'Administrators'
     }
-  }
+    default: {
+      $site_libdir_owner = 'root',
+      $site_libdir_group = '0'
+    }
 
-  mcollective::common::setting { 'libdir':
-    value => "${mcollective::site_libdir}:${mcollective::core_libdir}",
-  }
+    file { $mcollective::site_libdir:
+      ensure       => directory,
+      owner        => $site_libdir_owner,
+      group        => $site_libdir_group,
+      mode         => '0644',
+      recurse      => true,
+      purge        => true,
+      force        => true,
+      source       => [],
+      sourceselect => 'all',
+    }
 
-  mcollective::common::setting { 'connector':
-    value => $mcollective::connector,
-  }
+    if $mcollective::server {
+      # if we have a server install, reload when the plugins change
+      File[$mcollective::site_libdir] ~> Class['mcollective::server::service']
+    }
 
-  mcollective::common::setting { 'securityprovider':
-    value => $mcollective::securityprovider,
-  }
+    datacat_collector { 'mcollective::site_libdir':
+      before          => File[$mcollective::site_libdir],
+      target_resource => File[$mcollective::site_libdir],
+      target_field    => 'source',
+      source_key      => 'source_path',
+    }
 
-  mcollective::common::setting { 'collectives':
-    value => join(flatten([$mcollective::collectives]), ','),
-  }
+    datacat_fragment { 'mcollective::site_libdir':
+      target => 'mcollective::site_libdir',
+      data   => {
+        source_path => [ 'puppet:///modules/mcollective/site_libdir' ],
+      }
+    }
 
-  mcollective::common::setting { 'main_collective':
-    value => $mcollective::main_collective,
-  }
+    mcollective::common::setting { 'libdir':
+      value => "${mcollective::site_libdir}:${mcollective::core_libdir}",
+    }
 
-  mcollective::soft_include { [
-    "::mcollective::common::config::connector::${mcollective::connector}",
-    "::mcollective::common::config::securityprovider::${mcollective::securityprovider}",
-  ]:
-    start => Anchor['mcollective::common::config::begin'],
-    end   => Anchor['mcollective::common::config::end'],
-  }
+    mcollective::common::setting { 'connector':
+      value => $mcollective::connector,
+    }
 
-  anchor { 'mcollective::common::config::begin': }
-  anchor { 'mcollective::common::config::end': }
-}
+    mcollective::common::setting { 'securityprovider':
+      value => $mcollective::securityprovider,
+    }
+
+    mcollective::common::setting { 'collectives':
+      value => join(flatten([$mcollective::collectives]), ','),
+    }
+
+    mcollective::common::setting { 'main_collective':
+      value => $mcollective::main_collective,
+    }
+
+    mcollective::soft_include { [
+        "::mcollective::common::config::connector::${mcollective::connector}",
+        "::mcollective::common::config::securityprovider::${mcollective::securityprovider}",
+      ]:
+        start => Anchor['mcollective::common::config::begin'],
+        end   => Anchor['mcollective::common::config::end'],
+    }
+
+    anchor { 'mcollective::common::config::begin': }
+    anchor { 'mcollective::common::config::end': }
+  }
